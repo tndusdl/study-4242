34ea0ef1aeb46cb3f68dc70bca76436e683670ff
suyeon kim <ksy04120@naver.com>
2024-03-14 09:03:28 +0900

    first commit

---
+++ 한준이1-2.js
@@ -0,0 +1,12 @@
+function agecalc () {
+    var x = 22, result;
+    result = 23;
+
+    if (result === 21) {
+        var str = 'Correct, My age is ';
+        alert("Correct, My age is 21");
+    }
+    else {
+        alert("Try again");
+    }
+}
